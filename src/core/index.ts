d89ea3167a8c477b6488500295059ee2a5c1d5d3
yaochenghao <yaochenghao@bytedance.com>
2018-07-22 23:33:38 +0800

    add: render api

--- src/core/index.ts
+++ src/core/index.ts
@@ -9,16 +9,44 @@ export default class {
     private canvas: HTMLCanvasElement;
     private ctx: CanvasRenderingContext2D;
     private pixelRatio: number;
+    private data: number[];
 
     constructor(config: IConfig) {
         this.config = config;
-        this.default();
+        this.defaultValue();
 
         this.canvasInit();
     }
 
+    public update(data: number[]): void {
+        this.data = data;
+    }
+
+    public reset(): void {
+        this.canvas.height = this.canvas.height;
+    }
+
+    public render(): void {
+        const marginX: number = this.config.width / (this.data.length - 1);
+        const maxY: number = Math.max(...this.data);
+        const rateY: number = this.config.height / maxY;
+        this.reset();
+        this.ctx.save();
+        this.ctx.fillStyle = this.config.color;
+        this.ctx.beginPath();
+        this.ctx.moveTo(0, 0);
+
+        for (let i: nubmer = 0; i < this.data.length; i = i + 1) {
+            this.ctx.lineTo(i * marginX, this.data[i] * rateY);
+        }
+
+        this.ctx.fill();
+        this.ctx.closePath();
+        this.ctx.restore();
+    }
+
     // 参数默认值
-    private default(): void {
+    private defaultValue(): void {
         this.config.color = this.config.color || 'blue';
     }
 
@@ -33,6 +61,4 @@ export default class {
         this.canvas.style.width = `${this.config.width}px`;
         this.canvas.style.height = `${this.config.height}px`;
     }
-
-    private screenInit(): void {}
 }
