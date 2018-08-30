47bbcca2e012a1dad27ba59c833738111b0d556b
yaochenghao <yaochenghao@bytedance.com>
2018-08-30 14:56:25 +0800

    add: pie file

---
+++ src/core/pie.ts
@@ -0,0 +1,20 @@
+/**
+ * @desc pie chart (饼图)
+ */
+
+import Chart from '@/core/chart';
+
+// interface IConfig {
+//     a: number;
+// }
+//
+// class Config {
+//     public obj: IConfig
+//     constructor(obj: IConfig, def: IConfig) {
+//         this.obj = obj;
+//     }
+// }
+
+export default class Pie extends Chart {
+    constructor(config: IConfig) {}
+}
