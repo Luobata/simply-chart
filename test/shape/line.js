0feeb8eec8de80ea00c707c35d5e57906f0440ed
yaochenghao <yaochenghao@bytedance.com>
2018-10-30 21:35:10 +0800

    add: more line test unit

--- test/shape/line.js
+++ test/shape/line.js
@@ -26,32 +26,66 @@ describe('line unit:', () => {
             ...basicConifg,
             dom,
             canvas,
-            renderType: 'point',
         },
         attr: {
             ...attrConifg,
             shadowColor: 'yellow',
         },
     });
+    describe('basic confiig', () => {
+        it('basic confiig', () => {
+            assert.deepEqual(line.config, {
+                dom,
+                canvas,
+                width: 200,
+                height: 100,
+                padding: 10,
+                renderType: 'none',
+                renderTime: 200 / 60,
+                framePerSecond: 60,
+                renderCurve: 'ease-in-out',
+                color: 'blue',
+                lineWidth: 2,
+                point: false,
+                pointRadius: 3,
+                pointFill: false,
+                smooth: false,
+                shadowColor: 'yellow',
+                innerWidth: 180,
+                innerHeight: 80,
+            });
 
-    assert.deepEqual(line.config, {
-        dom,
-        canvas,
-        width: 200,
-        height: 100,
-        padding: 10,
-        renderType: 'point',
-        renderTime: 200 / 60,
-        framePerSecond: 60,
-        renderCurve: 'ease-in-out',
-        color: 'blue',
-        lineWidth: 2,
-        point: false,
-        pointRadius: 3,
-        pointFill: false,
-        smooth: false,
-        shadowColor: 'yellow',
-        innerWidth: 180,
-        innerHeight: 80,
+            line.point();
+            assert.deepEqual(line.config.point, true);
+
+            line.fill();
+            assert.deepEqual(line.config.pointFill, true);
+        });
+
+        it('data update:', () => {
+            line.update([1, 2.5, 3, 2.5, 1]);
+            assert.deepEqual(line.pointList, [
+                {
+                    x: 0,
+                    y: 0,
+                },
+                {
+                    x: 45,
+                    y: 60,
+                },
+                {
+                    x: 90,
+                    y: 80,
+                },
+                {
+                    x: 135,
+                    y: 60,
+                },
+                {
+                    x: 180,
+                    y: 0,
+                },
+            ]);
+        });
     });
 });
