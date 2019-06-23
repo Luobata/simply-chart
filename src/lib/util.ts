1f1134a247b78d20c78ec275734b10fe2780750a
yaochenghao <yaochenghao@bytedance.com>
2019-06-23 16:35:37 +0800

    add: 增加公共delay 延迟执行函数

---
+++ src/lib/util.ts
@@ -0,0 +1,26 @@
+/**
+ * @description util
+ */
+
+export const delay: Function = (timeTicket: number, fn: Function): Function => {
+    // let start!: number;
+    let timer: number;
+
+    return (): void => {
+        // const now: number = new Date().getTime();
+        if (!timer) {
+            // start = new Date().getTime();
+            timer = setTimeout(() => {
+                fn();
+            }, timeTicket);
+        } else {
+            clearTimeout(timer);
+            timer = setTimeout((): void => {
+                fn();
+                clearTimeout(timer);
+                timer = null;
+                // start = 0;
+            }, timeTicket);
+        }
+    };
+};
